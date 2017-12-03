547d01bf6fb93e9bdbff46dc8ebe3ea50d6cc124
tomon9086 <tomo.mouse.9086@gmail.com>
2017-12-03 19:23:15 +0900

    add:header-footer

--- client/components/Root.jsx
+++ client/components/Root.jsx
@@ -1,14 +1,23 @@
 const React = require("react")
 
+const style = require("./Root.css")
+
 const Hello = require("./Hello.jsx")
 const Counter = require("../containers/Counter.js")
 
 class Root extends React.Component {
 	render() {
 		return (
-			<div>
-				<Hello />
-				<Counter />
+			<div className={ style.container }>
+				<div className={ style.header }><h3>React Practice</h3></div>
+				<div className={ style.content }>
+					<Hello />
+					<Counter />
+				</div>
+				<div className={ style.footer }>
+					
+					<div className={ style.copyright }>© 2017 <a href="https://github.com/tomon9086/React-practice" target="_blank">tomon9086</a></div>
+				</div>
 			</div>
 		)
 	}
